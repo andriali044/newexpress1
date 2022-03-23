8b36c2b8d323e0a533f49744c96bded2c3cb76f9
andriali044 <andriali044gmail.com>
2022-03-23 20:55:32 +0700

    new project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+      nama: 'Andri',
+      email: 'andriali044@gmail.com',
+      noHP: '085925153931',
+  });
+});
+app.get('/about', (req, res) => {
+  res.send('halaman pertama');
+});
+
+app.use('/', (req, res) => {
+    res.send('<h1>gada isinya</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
